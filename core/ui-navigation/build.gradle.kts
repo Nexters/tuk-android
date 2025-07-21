51160cb2c457c1da9ca665b71d5eed9c134e5b4f
eshc123 <>
2025-07-21 19:41:12 +0900

    FEAT: core:ui-navigation 모듈 추가

---
+++ core/ui-navigation/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    alias(libs.plugins.tuk.android.library)
+}
+
+android {
+    namespace = "com.plottwist.core.ui.navigation"
+}
+
+dependencies {
+    implementation(libs.core.ktx)
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.test.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+}
