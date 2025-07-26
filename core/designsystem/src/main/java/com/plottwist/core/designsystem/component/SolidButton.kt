6ce6d58e654eadf1b50be7ef90bbb7f1556c3955
eshc123 <>
2025-07-26 23:50:52 +0900

    FEAT: SolidButton 추가

---
+++ core/designsystem/src/main/java/com/plottwist/core/designsystem/component/SolidButton.kt
@@ -0,0 +1,52 @@
+package com.plottwist.core.designsystem.component
+
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.plottwist.core.designsystem.foundation.type.TukPretendardTypography
+
+@Composable
+fun SolidButton(
+    text: String,
+    containerColor: Color,
+    contentColor: Color,
+    modifier: Modifier = Modifier,
+    trailingIcon: (@Composable () -> Unit)? = null
+) {
+    Row(
+        modifier = modifier
+            .padding(
+                vertical = 8.dp,
+                horizontal = 10.dp
+            )
+            .clip(
+                shape = RoundedCornerShape(100)
+            )
+            .background(
+                color = containerColor,
+                shape = RoundedCornerShape(100)
+            ),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        Text(
+            text = text,
+            style = TukPretendardTypography.body14M,
+            color = contentColor
+        )
+
+        if(trailingIcon != null) {
+            trailingIcon()
+        }
+    }
+}
