da7c300cca1d288d796fa4548ef002914d7c3c71
eshc123 <>
2025-07-27 00:35:45 +0900

    FEAT: 로그인 화면 앱바 추가

--- feature/login/src/main/java/com/plottwist/feature/login/LoginScreen.kt
+++ feature/login/src/main/java/com/plottwist/feature/login/LoginScreen.kt
@@ -2,35 +2,20 @@ package com.plottwist.feature.login
 
 import android.app.Activity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Text
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.plottwist.core.auth.provider.di.AuthProviderEntryPoint
+import com.plottwist.core.designsystem.R
+import com.plottwist.core.designsystem.component.TukTopAppBar
+import com.plottwist.core.ui.component.StableImage
 import dagger.hilt.android.EntryPointAccessors
 import org.orbitmvi.orbit.compose.collectSideEffect
 
@@ -40,8 +25,6 @@ fun LoginScreen(
     modifier: Modifier = Modifier,
     viewModel: LoginViewModel = hiltViewModel()
 ) {
-    Text("로그인 화면")
-
     val context = LocalContext.current
     val activity = context as Activity
     val coroutineScope = rememberCoroutineScope()
@@ -74,46 +57,55 @@ fun LoginScreen(
         }
     }
 
-    Box(
-        modifier = Modifier
+    LoginScreen(
+        modifier = modifier
             .fillMaxSize()
-            .background(Color.Gray)
-    ) {
-        Button(
-            onClick = {
-                viewModel.handleAction(LoginAction.ClickGoogleLogin)
-            },
-            modifier = Modifier
-                .align(Alignment.BottomCenter)
-                .padding(start = 16.dp, end = 16.dp, bottom = 32.dp)
-                .fillMaxWidth()
-                .height(48.dp),
-            colors = ButtonDefaults.buttonColors(
-                containerColor = Color.Black
-            ),
-            shape = RoundedCornerShape(8.dp),
-            contentPadding = PaddingValues()
-        ) {
+            .background(Color.White),
+        onCloseClicked = onBack
+    )
+}
 
-            Row(
-                modifier = Modifier.fillMaxSize(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.Center
-            ) {
-                Icon(
-                    painter = painterResource(id = com.plottwist.core.designsystem.R.drawable.ic_mypage),
-                    contentDescription = "Apple Login",
-                    tint = Color.White,
-                    modifier = Modifier.size(20.dp)
-                )
-                Spacer(modifier = Modifier.width(8.dp))
-                Text(
-                    text = "로그인",
-                    color = Color.White,
-                    fontSize = 16.sp,
-                    fontWeight = FontWeight.SemiBold
-                )
-            }
+@Composable
+private fun LoginScreen(
+    onCloseClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column (
+        modifier = modifier
+    ) {
+        LoginAppBar(
+            onCloseClicked = onCloseClicked
+        )
+
+    }
+}
+
+@Composable
+fun LoginAppBar(
+    onCloseClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TukTopAppBar(
+        modifier = modifier,
+        actionButtons = {
+            TopAppBarCloseButton(
+                onCloseClicked = onCloseClicked
+            )
         }
+    )
+}
+
+@Composable
+fun TopAppBarCloseButton(
+    onCloseClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        modifier = modifier,
+        onClick = onCloseClicked
+    ) {
+        StableImage(
+            drawableResId = R.drawable.ic_close
+        )
     }
 }
