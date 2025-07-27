f4f2aea0db925de395d2267dcf746aa9f6230b2c
안나영 <dpswpfskdud@naver.com>
2025-07-27 11:44:56 +0900

    FEAT: 모임 초대장 수신 ui 구현

--- feature/join_gathering/build.gradle.kts
+++ feature/join_gathering/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
-    alias(libs.plugins.com.android.library)
-    alias(libs.plugins.org.jetbrains.kotlin.android)
+    alias(libs.plugins.tuk.android.library)
     alias(libs.plugins.tuk.feature)
 }
 
