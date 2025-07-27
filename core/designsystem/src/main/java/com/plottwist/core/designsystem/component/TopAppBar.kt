aabe60d9b5bf34503591c39a955e858c160c8285
안나영 <dpswpfskdud@naver.com>
2025-07-27 10:13:26 +0900

    FEAT: 모임 초대 요청 화면 ui

--- core/designsystem/src/main/java/com/plottwist/core/designsystem/component/TopAppBar.kt
+++ core/designsystem/src/main/java/com/plottwist/core/designsystem/component/TopAppBar.kt
@@ -6,16 +6,35 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun TukTopAppBar(
+    title: String? = null,
     modifier: Modifier = Modifier,
     actionButtons: (@Composable RowScope.() -> Unit)? = null,
 ) {
+    title?.let {
+        Text(
+            text = it,
+            modifier = Modifier
+                .padding(start = 20.dp),
+            style = TextStyle(
+                fontSize = 16.sp,
+                fontWeight = FontWeight.SemiBold,
+                color = Color.Black
+            )
+        )
+    }
+
     Row(
         modifier = modifier
             .height(64.dp)
