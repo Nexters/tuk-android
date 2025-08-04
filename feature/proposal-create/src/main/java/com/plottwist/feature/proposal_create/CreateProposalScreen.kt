0414ee30a1f9149931af96d0ebbfb4433cf0ad6d
eshc123 <>
2025-08-04 20:26:02 +0900

    FEAT: 모임 제안 생성 화면 디자인 시스템 버튼 적용

--- feature/proposal-create/src/main/java/com/plottwist/feature/proposal_create/CreateProposalScreen.kt
+++ feature/proposal-create/src/main/java/com/plottwist/feature/proposal_create/CreateProposalScreen.kt
@@ -2,7 +2,6 @@ package com.plottwist.feature.proposal_create
 
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,14 +10,11 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
@@ -27,8 +23,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.plottwist.core.designsystem.R
+import com.plottwist.core.designsystem.component.TukSolidButton
+import com.plottwist.core.designsystem.component.TukSolidButtonType
 import com.plottwist.core.designsystem.component.TukTopAppBar
-import com.plottwist.core.designsystem.foundation.type.TukPretendardTypography
 import com.plottwist.core.designsystem.foundation.type.TukSerifTypography
 import com.plottwist.core.ui.component.StableImage
 import com.plottwist.core.ui.component.TopAppBarCloseButton
@@ -107,11 +104,11 @@ private fun CreateProposalScreen(
                 )
             }
 
-            SolidButton(
+            TukSolidButton(
                 modifier = Modifier.fillMaxWidth()
                     .padding(horizontal = 20.dp, vertical = 16.dp),
-                label = stringResource(R.string.create_proposal_propose),
-                state = if(isGatheringSelected) ButtonState.ENABLED else ButtonState.DISABLED,
+                text = stringResource(R.string.create_proposal_propose),
+                buttonType = if(isGatheringSelected) TukSolidButtonType.ACTIVE else TukSolidButtonType.DISABLED,
                 onClick = {
                     // TODO
                 }
@@ -160,36 +157,6 @@ fun CreateProposalTitle(
         text = stringResource(R.string.create_proposal_title),
         style = TukSerifTypography.title24M
     )
-}
-
-@Composable
-fun SolidButton(
-    label: String,
-    state: ButtonState,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    shape: RoundedCornerShape = RoundedCornerShape(15.dp)
-) {
-    Text(
-        modifier = modifier
-            .height(52.dp)
-            .background(
-                color = if (state == ButtonState.ENABLED) Color(0xFFFF3838)
-                else Color(0xFFEAEAEA),
-                shape = shape
-            )
-            .clip(shape)
-            .clickable(state != ButtonState.DISABLED) {
-                onClick()
-            }.wrapContentSize(Alignment.Center),
-        text = label,
-        style = TukPretendardTypography.body16M,
-        color = Color(0xFFFFFFFF)
-    )
-}
-
-enum class ButtonState {
-    ENABLED, DISABLED
 }
 
 
