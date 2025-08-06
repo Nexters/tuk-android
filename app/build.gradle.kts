c0f4f8186a0924914fb8cce2513b2f7849d9012f
eshc123 <>
2025-08-06 12:14:08 +0900

    FEAT: FCM 서비스 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.tuk.android.application)
     alias(libs.plugins.tuk.android.application.compose)
     alias(libs.plugins.tuk.android.application.hilt)
+    alias(libs.plugins.google.service)
 }
 
 android {
@@ -16,5 +17,8 @@ dependencies {
     implementation(project(":core:data"))
     implementation(project(":core:auth-provider"))
     implementation(project(":core:weburl"))
+    implementation(project(":core:push"))
     testImplementation(libs.junit)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.messaging)
 }
