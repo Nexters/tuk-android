5d7963849f985c970bdd3b27db96a8b417d250be
eshc123 <eshc.joon@gmail.com>
2025-08-09 03:45:25 +0900

    FEAT: 온보딩 화면 수정

--- core/network/src/main/java/com/plottwist/core/network/service/OnboardingService.kt
+++ core/network/src/main/java/com/plottwist/core/network/service/OnboardingService.kt
@@ -4,9 +4,10 @@ import com.plottwist.core.network.model.onboarding.UpdateOnboardingInfoRequest
 import com.plottwist.core.network.model.onboarding.UpdateOnboardingInfoResponse
 import retrofit2.http.Body
 import retrofit2.http.PATCH
+import retrofit2.http.PUT
 
 interface OnboardingService {
-    @PATCH("/api/v1/onboarding")
+    @PUT("/api/v1/onboarding")
     suspend fun updateOnboardingInfo(
         @Body request: UpdateOnboardingInfoRequest
     ): UpdateOnboardingInfoResponse
