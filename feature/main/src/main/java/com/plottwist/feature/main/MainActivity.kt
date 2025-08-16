85efa4ee12dc17f6dac55ab2314a7b1142d48014
eshc123 <eshc.joon@gmail.com>
2025-08-17 01:57:29 +0900

    FEAT: 딥링크 처리

--- feature/main/src/main/java/com/plottwist/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/plottwist/feature/main/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.plottwist.feature.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.core.net.toUri
 import com.plottwist.feature.main.ui.theme.TukTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -13,6 +15,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        handleDeepLink(intent)
         setComposeContent()
     }
 
@@ -22,6 +25,16 @@ class MainActivity : ComponentActivity() {
                 TukApp()
             }
         }
+    }
+
+    private fun handleDeepLink(
+        intent: Intent,
+    ) {
+        val deepLink = intent.extras?.getString("deepLink") ?: return
+
+        startActivity(Intent(Intent.ACTION_VIEW).apply {
+            data = deepLink.toUri()
+        })
     }
 
     companion object {
